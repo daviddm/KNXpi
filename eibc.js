ef843b7b50f5a5026d8978cd1cf18f6d6492d776
daviddm <david.de.meijere@gmail.com>
2013-03-27 17:32:01 +0100

    structered calls to eibd

--- eibc.js
+++ eibc.js
@@ -9,8 +9,12 @@ EIBClient.opts = {
 	port: '6720'
 };
 
-EIBClient.groupswrite = function(){
-	eibc.test();
+EIBClient.groupswrite = function(addr, val, callback) {
+	if(typeof addr === 'undefined'){return}
+	if(typeof val === 'undefined'){return}
+	eibc.connect(EIBClient.opts, function() {
+		eibc.groupswrite(addr, val, callback);
+	});
 };
 
 var eibc = function(){
@@ -18,20 +22,44 @@ var eibc = function(){
 	var que = new Array();
 	var conn;
 	
-	that.execute = function() {
-		var conn = eibd.socketRemote(eibd_opts, function() {
-			if(eibd.socket) {
-				console.log('EIBD Connected');
-			} else {
-				console.log('EIBD Failed');
-			}
+	that.connect = function(opts, callback) {
+		conn = eibd();
+		conn.socketRemote(opts, function() {
+			console.log('EIBD Connected');
+			callback();
 		});
-		conn.onData = function(data){
+		conn.socket.on('data', function(data){
 			if(que.length > 0) {
 				
 			}
-			console.log(data);
-		};
+			console.log('DATA: ' + data.toString());
+		});
+		conn.socket.on('end', function(){
+			console.log('EIBD Closed the connection');
+		});
+		conn.socket.on('close', function(){
+			console.log('EIBD Transmission error');
+		});
+	};
+	
+	/**
+	 * addr - in format x/x/x
+	 * value - 1|0
+	 */
+	that.groupswrite = function(addr, val, callback) {
+		if(typeof conn === 'undefined'){console.log('Trying to send too early');return false;}
+		conn.openTGroup(conn.str2addr(addr), 0, function (err) {
+			if(err) {
+				console.log('ERROR opening T_Group');
+			}
+			var data = new Array(2);
+			data[0] = 0;
+			data[1] = 0x80 | val;
+			conn.sendAPDU(data, function() {
+				console.log('Sent "' + val + '" to ' + addr)
+				if(callback) callback();
+			});
+		});
 	};
 	
 	that.test = function() {
